4639466e2420085bcf401c915820580646514f39
jashment <jordanashment@gmail.com>
2019-01-23 15:36:25 -0700

    Added various folders and files

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const dev_db_url = 'mongodb+srv://jashment:Password1!@nodeserver1-ushmx.mongodb.net/admin'
+
+//const mongoDB = proces.env.MONGODB_URI || 
+
+const product = require('./routes/product.route')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/products', product)
 
 const port = 5775
 
---
+++ controllers/product.controller.js
@@ -0,0 +1,20 @@
+const Product = require('../models/product.model')
+
+exports.test = (req, res, next) => {
+    res.send('Greetings from the test controller')
+}
+
+exports.product_create = (req, res, next) => {
+    let product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+    })
+
+    product.save((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.send('Product created successfully')
+    })
+}
+
