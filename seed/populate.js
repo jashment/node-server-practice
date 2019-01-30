97a9fe24a1fc7b0727d576c4a2ec5c7e06adfd83
jashment <jordanashment@gmail.com>
2019-01-30 14:59:12 -0700

    Fixing populate.js

--- seed/populate.js
+++ seed/populate.js
@@ -1,14 +1,13 @@
 const fetch = require('node-fetch')
 
-fetch('https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=25')
+fetch('https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=25') //https://pokeapi.co/api/v2/pokemon?limit=25
   .then(function(response) {
     return response.json()
   })
   .then(function(myJson) {
     const pokeArray = myJson.results
-
-    pokeArray.foreach(pokemon => {
-        postData(`http://localhost:5775/products/pokemon`, {answer: 42})
+    pokeArray.forEach(pokemon => {
+        postData(`http://localhost:5775/products/pokemon`, pokemon)
         .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
         .catch(error => console.error(error));
     })
@@ -18,16 +17,11 @@ fetch('https://pokeapi-215911.firebaseapp.com/api/v2/pokemon?limit=25')
     // Default options are marked with *
       return fetch(url, {
           method: "POST", // *GET, POST, PUT, DELETE, etc.
-          mode: "no-cors", // no-cors, cors, *same-origin
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "omit", // include, *same-origin, omit
           headers: {
               "Content-Type": "application/json",
               // "Content-Type": "application/x-www-form-urlencoded",
           },
-          redirect: "follow", // manual, *follow, error
-          referrer: "no-referrer", // no-referrer, *client
           body: JSON.stringify(data), // body data type must match "Content-Type" header
       })
-      .then(response => response.json()); // parses response to JSON
+      .then(response => response.text()); // parses response to JSON
     }
